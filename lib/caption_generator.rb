bb2e7d8be22787af70c85f1135b0ee19d18d1819
Matt Condon <mattgcondon@gmail.com>
2017-01-28 12:37:31 -0500

    feat: add caption

--- lib/caption_generator.rb
+++ lib/caption_generator.rb
@@ -2,7 +2,8 @@ class CaptionGenerator
   def self.random_caption
     [
       "when you get a horror movie ad that you can't skip",
-      "when you bit the inside of your cheek and then it swells and you enter a vicious cycle of accidental biting and tears"
+      "when you bit the inside of your cheek and then it swells and you enter a vicious cycle of accidental biting and tears",
+      "when a song comes on shuffle that's more intense than your current mood is ready for"
     ].sample
   end
 end
